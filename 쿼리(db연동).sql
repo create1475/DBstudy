63f79565f33a16cef6cd8af70dd3315d23c36574
create1475 <107625673+create1475@users.noreply.github.com>
2022-08-08 18:12:46 +0900

    2022/0808
    2022/0808

---
+++ 쿼리(db연동).sql
@@ -0,0 +1,16 @@
+USE servletex;
+
+CREATE TABLE t_board(
+
+	articleNO INT(100) UNSIGNED NOT NULL,
+	parentNO INT(100) DEFAULT 0,
+	title VARCHAR(500) NOT NULL,
+	content VARCHAR(4000),
+	imageFileName VARCHAR(30),
+	writeDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+	id VARCHAR(20),
+	FOREIGN KEY(id) REFERENCE t_member(id),
+	PRIMARY KEY(`articleNO`));
+	
+	
+
